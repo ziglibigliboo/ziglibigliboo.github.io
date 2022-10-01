b2bd5ded112adf617128e93e513e3acbc765ac85
ziglibigliboo <68416275+ziglibigliboo@users.noreply.github.com>
2022-10-01 10:46:28 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,28 +1,26 @@
 (function ($) {
-
   "use strict";
 
-    // PRE LOADER
-    $(window).load(function(){
-      $('.preloader').fadeOut(1000); // set duration in brackets    
-    });
+  // PRE LOADER
+  $(window).load(function () {
+    $(".preloader").fadeOut(1000); // set duration in brackets
+  });
 
+  // MENU
+  $(".menu-burger").on("click", function () {
+    $(".menu-bg, .menu-items, .menu-burger").toggleClass("fs");
+    $(".menu-burger").text() == "☰"
+      ? $(".menu-burger").text("✕")
+      : $(".menu-burger").text("☰");
+  });
 
-    // MENU
-    $('.menu-burger').on('click', function() {
-      $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
-      $('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
-    });
-
-
-    // ABOUT SLIDER
-    $('body').vegas({
-        slides: [
-            { src: 'images/slide-image01.png' },
-            { src: 'images/slide-image02.png' }
-        ],
-        timer: false,
-        transition: [ 'zoomOut', ]
-    });
-
+  // ABOUT SLIDER
+  $("body").vegas({
+    slides: [
+      { src: "images/slide-image01.png" },
+      { src: "images/slide-image02.png" },
+    ],
+    timer: false,
+    transition: ["zoomOut"],
+  });
 })(jQuery);
