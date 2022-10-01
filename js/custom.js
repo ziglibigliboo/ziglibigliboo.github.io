a671b270f0a98d6018a89ed1533d316b4fecb47e
ziglibigliboo <68416275+ziglibigliboo@users.noreply.github.com>
2022-10-01 09:57:28 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -18,8 +18,8 @@
     // ABOUT SLIDER
     $('body').vegas({
         slides: [
-            { src: 'images/slide-image01.jpg' },
-            { src: 'images/slide-image02.jpg' }
+            { src: 'images/slide-image01.png' },
+            { src: 'images/slide-image02.png' }
         ],
         timer: false,
         transition: [ 'zoomOut', ]
